49e5a956d2b67c078ebbd0d53ed9329fd50626f8
Rjrtriky <rjuarez-@student.42madrid.com>
2026-02-26 11:28:10 +0100

    Add files via upload

--- src/core/push_swap.c
+++ src/core/push_swap.c
@@ -6,7 +6,43 @@
 /*   By: rjuarez- <rjuarez-@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 14:05:21 by rjuarez-          #+#    #+#             */
-/*   Updated: 2026/02/24 01:15:12 by rjuarez-         ###   ########.fr       */
+/*   Updated: 2026/02/24 14:41:18 by rjuarez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "data.h"
+#include "moves.h"
+#include "push_swap.h"
+
+t_data	*ft_fill(int argc, char *argv[]);
+
+t_data	*ft_fill(int argc, char *argv[])
+{
+	int		i;
+	int		err;
+	t_data	data;
+
+	data = ft_data_new();
+	i = argc;
+	while ((i-- > 0) && (err == 0))
+	{
+		err = ft_data_stack_a_add(data, argv[i]);
+		imprimir(data);
+	}
+	if (err == 1)
+	{
+		ft_data_free(data);
+		return (NULL);
+	}
+	return (data);
+}
+
+int main(int argc, char *argv[])
+{
+	t_data	data;
+
+	data = ft_fill(argc, argv);
+	ft_data_free(data);
+
+	return (0);
+}
